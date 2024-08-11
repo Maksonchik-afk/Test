317d3b2c5b5be0a54640dab2f8c76b1dd2f15465
Maksonchik-afk <kolchinmaximili@gmail.com>
2024-08-11 21:08:07 +0300

    Test

---
+++ homeworks/HW_lesson_5/HW_5.2.py
@@ -0,0 +1,36 @@
+while True:
+    print('Вас вітає найпростіший калькулятор')
+
+    number_1 = float(input('Введіть перше число: '))
+    math_operation = input('Введіть математичну дію, / , *, -, + : ')
+    number_2 = float(input('Введіть друге число: '))
+
+    result = None
+
+    if math_operation == '+':
+        result = number_1 + number_2
+        print(result)
+
+    elif math_operation == '-':
+        result = number_1 - number_2
+        print(result)
+
+    if math_operation == '*':
+        result = number_1 * number_2
+        print(result)
+
+    elif math_operation == '/':
+        if number_2 != 0:
+            result = number_1 / number_2
+            print(result)
+        else:
+            print('На нуль ділити не можна!!!')
+
+    answer_1 = input('Чи хочете продовжити користування калькулятором? ')
+
+    if answer_1 == 'yes':
+        continue
+    else:
+        print('Дякую за користування калькулятором!')
+        break
+
